aed9c10d59830f038fc8aa8d294b5d886e332a5e
Erhan Justice <bountyx@gmail.com>
2022-02-20 23:43:48 -0800

    Fix tag issues

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
       The Confetti Button component is used to give a little spark to the button
       components. Clicking the button will animate the icons, creating a fun
       feedback effect. See
-      <a href="https://codyhouse.co/ds/components/info/confetti-button" >info page</a >.
+      <a href="https://codyhouse.co/ds/components/info/confetti-button">info page</a>.
     </p>
-    <p><confetti-btn class="btn--lg">Hello World!</confetti-btn></p>
+    <confetti-btn class="btn--lg">Hello World!</confetti-btn>
   </div>
 </template>
